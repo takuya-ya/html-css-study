bed274dd76e835e0c5014111255b12ae6f72d53b
Your Name <your-email@example.com>
2025-03-14 16:45:34 +0900

    9問目

---
+++ practice/at.php
@@ -0,0 +1,14 @@
+<?php
+
+$s = 'dreamerase';
+
+public function coder($s, $temp = '') {
+    $words = ["dream", "dreamer", "erase", "eraser"];
+
+    // 先頭から単語を見つけてそれを追加していく
+    // 目標の文字列の先頭単語と、$wordsの文字数を比較して抽出 substrで
+    
+
+    
+}
+coder()
